5a61e847aa592578353d66dd7a70f1f2e32a619f
Piotr Janiszek <247678@edu.p.lodz.pl>
2024-12-02 00:27:42 +0100

    Dodanie dokładności do milisekund dla logowania

--- Zadanie2/main.py
+++ Zadanie2/main.py
@@ -19,7 +19,7 @@ def setup_logging(log_level):
         raise ValueError('Invalid log level: %s' % log_level)
 
     logging.basicConfig(level=numeric_level,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        format='%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s',
                         datefmt='%d/%m/%Y %H:%M:%S',
                         filename='chase.log')
 
