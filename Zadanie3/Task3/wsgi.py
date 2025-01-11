88402ea0bdcfcc4935736bdbf05bb1101d5f3dfd
Piotr Janiszek <247678@edu.p.lodz.pl>
2025-01-11 08:32:32 +0100

    Poprawa nazwy projektu

--- Zadanie3/Task3/wsgi.py
+++ Zadanie3/Task3/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Zadanie3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Task3.settings')
 
 application = get_wsgi_application()
